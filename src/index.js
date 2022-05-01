4126e506b9dbeabb69d65faaa9f1c5cdbe47634e
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-05-01 12:37:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
-
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("I've been clicked.")} />
   </div>,
   document.getElementById('root')
 );
